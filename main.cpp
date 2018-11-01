4525936cf694501666e4c2f2a958609562481ec4
Enrique Pernia <Epernia96@gmail.com>
2018-11-02 00:03:43 +0100

    Thread creating

--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
 #include <iostream>
 #include <thread>
+#include <string>
 using namespace std;
-void main() { std::cout << "Hello, world, from Visual C++!" << std::endl; }
+int main(int argc, char** argv) {
+  std::cout << "Have " << argc << " arguments:" << std::endl;
+  for (int i = 0; i < argc; ++i) {
+    std::cout << argv[i] << std::endl;
+  }
+  std::thread t1{std::bind(f, even)};
+  std::thread t2{std::bind(f, odd)};
+
+ /* Espera que finalicen */
+ t1.join();
+ t2.join();
+}
