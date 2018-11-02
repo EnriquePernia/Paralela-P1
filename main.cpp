fd8965696c8da0db5edf8a5a0ade97f3e7a7f9f0
Enrique Pernia <Epernia96@gmail.com>
2018-11-02 10:35:48 +0100

    First session

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <thread>
-#include <string>
 #include <array>
 using namespace std;
 
@@ -8,10 +7,19 @@ using namespace std;
 
 // void createThreads(int arrLength,char option, char numThreads);
 
-int * createThreads(int arrLength,char option, char numThreads){
-  int arrayNum[arrLength];
+int sum(int * arr, int count){ //Funcion que suma los elem dek array
+  int sumatory;
+  for (int i = 0; i < count; i++) {
+    sumatory+= *(arr + i);
+  }
+  return sumatory;
+}
+
+double * createThreads(int arrLength,char option, char numThreads){ //Funcion que crea el array(de momento)
+  double * arrayNum= (double*)malloc(arrLength * sizeof(double)); //Reservando espacio para el array
+  // int arrayNum[arrLength];
   for (int i = 0; i < arrLength; i++) {
-    arrayNum[i]=i+1;
+    arrayNum[i]=i+1; //Asignando valores al array
   }
   return  arrayNum  ;
 }
@@ -19,14 +27,18 @@ int * createThreads(int arrLength,char option, char numThreads){
 
 int main(int argc, char** argv) {
   std::cout << "Have " << argc << " arguments:" << std::endl;
-  for (int i = 0; i < argc; ++i) {
+  for (int i = 0; i < argc; ++i) {  //Mostramos argumentos
     std::cout << argv[i] << std::endl;
   }
-  int *z;
-  z = createThreads(stoi(argv[1]),*argv[2],*argv[3]);
-  std::cout << *(z + 1) << std::endl;
-  std::thread t1{std::bind(f, even)};
-  std::thread t2{std::bind(f, odd)};
+  double *workingArray;  //Array con el que trabajamos
+  workingArray = createThreads(stoi(argv[1]),*argv[2],*argv[3]);
+  std::cout << *(z + 1) << std::endl; //Mostrar primer elemento
+
+  int *a1; //array a pasar a cada thread
+  int *a2;
+
+  // std::thread t1{(f, even)};
+  // std::thread t2{(f, odd)};
 
  /* Espera que finalicen */
  // t1.join();
